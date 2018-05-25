63c388b1f5a3b165d0d5b47bbb644f6612694931
RuMaxwell <935906960@qq.com>
2018-05-25 09:47:01 +0800

    Initial commit on GitHub
    A full lexer is finished.

--- hello-world.cl
+++ hello-world.cl
@@ -1,10 +1,60 @@
-add something here?
 (*
     This is a Cool hello-world program.
 
     Implementer: Ruthenium Maxwell
         in 2018
 *)
+
+-- Standard Cool list
+class List
+{
+    null () : Bool
+    {
+        true
+    };
+
+    car () : String
+    {
+        abort()
+    };
+
+    cdr () : List
+    {
+        abort()
+    };
+
+    cons (x : String) : List
+    {
+        (new Cons).init(x, self)
+    };
+};
+
+class Cons inherits List
+{
+    head : String;
+    tail : List;
+
+    null () : Bool
+    {
+        false
+    };
+
+    car () : String
+    {
+        head
+    };
+
+    cdr () : List
+    {
+        tail
+    };
+
+    init (x : String, xs : List) : List
+    {{
+        head <- x;
+        tail <- xs;
+    }};
+};
 
 -- Program main class
 class Main inherits IO
--- lexer.py
+++ lexer.py
@@ -1,34 +1,30 @@
-states = [
-    "Normal",
-    "Identifier",
-    "Integer",
-    "Floating",
-    "String",
-    "Dash",
-    "LineComment",
-    "LParen",
-    "BlockComment",
-    "BlockCmtEnd?",
-    "Error"
-]
+"""
+    A lexer of Cool.
+"""
 
 def otherwise(_):
     return True
 
+
 def isAlpha(c):
     return 'A' <= c <= 'Z' or 'a' <= c <= 'z'
+
 
 def isNumber(c):
     return '0' <= c <= '9'
 
+
 def isAlphaNum(c):
     return isNumber(c) or isAlpha(c)
+
 
 def isIdHead(c):
     return isAlpha(c) or c == '_'
 
+
 def isIdBody(c):
     return isAlphaNum(c) or c == '_'
+
 
 stateTrans = {
     "Normal": [
@@ -36,8 +32,57 @@ stateTrans = {
         [isIdHead, "Identifier"],
         [lambda x: x == '"', "String"],
         [lambda x: x == '-', "Dash"],
+        [lambda x: x == '.', "Dispatch"],
+        [lambda x: x == '@', "BasePointer"],
+        [lambda x: x == '~', "Tilde"],
+        [lambda x: x == '*', "Multiplication"],
+        [lambda x: x == '/', "Division"],
+        [lambda x: x == '+', "Addition"],
+        [lambda x: x == '<', "Lower"],
+        [lambda x: x == '=', "Equal"],
         [lambda x: x == '(', "LParen"],
+        [lambda x: x == ')', "RParen"],
+        [lambda x: x == '{', "LBrace"],
         [otherwise, "Normal"]
+    ],
+    "LBrace": [
+        [otherwise, "EndOfToken"]
+    ],
+    "Dispatch": [
+        [otherwise, "EndOfToken"]
+    ],
+    "BasePointer": [
+        [otherwise, "EndOfToken"]
+    ],
+    "Tilde": [
+        [otherwise, "EndOfToken"]
+    ],
+    "Multiplication": [
+        [otherwise, "EndOfToken"]
+    ],
+    "Division": [
+        [otherwise, "EndOfToken"]
+    ],
+    "Addition": [
+        [otherwise, "EndOfToken"]
+    ],
+    "Lower": [
+        [lambda x: x == '-', "Assignment"],
+        [lambda x: x == '=', "LessEqual"],
+        [otherwise, "EndOfToken"]
+    ],
+    "Equal": [
+        [lambda x: x == '>', "CaseArrow"],
+        [otherwise, "EndOfToken"]
+    ],
+    "Assignment": [
+        [otherwise, "EndOfToken"]
+    ],
+    "LessEqual": [
+        [otherwise, "EndOfToken"]
+    ],
+    "CaseArrow": [
+        [otherwise, "EndOfToken"]
     ],
     "Identifier": [
         [isIdBody, "Identifier"],
@@ -83,10 +128,14 @@ stateTrans = {
     "BlockCmtEnd": [
         [otherwise, "EndOfToken"]
     ],
+    "RParen": [
+        [otherwise, "EndOfToken"]
+    ],
     "Error": [
         [otherwise, "Error"]
     ]
 }
+
 
 def refreshState(state, x):
     global stateTrans
@@ -99,13 +148,15 @@ def refreshState(state, x):
 
     raise Exception("Unknown problem")
 
+
 charbuff = ' '
 tokenbuff = ""
+
 
 def readToken(state, s):
     global charbuff, tokenbuff
     if s == "":
-        return ("EOF", "")
+        return ("EOF", "", False)
 
     x = s[0]
 
@@ -114,6 +165,7 @@ def readToken(state, s):
     newstate = refreshState(state, x)
 
     token = None
+    endoftok = False
 
     """if newstate == "Normal":
         if state != "Normal":
@@ -126,13 +178,44 @@ def readToken(state, s):
     if newstate == "Normal":
         tokenbuff = ""
     elif newstate == "EndOfToken":
+        endoftok = True
         newstate = "Normal"
         token = (state, tokenbuff[:-1])
         tokenbuff = ""
     else:
         token = None
 
-    return (newstate, token)
+    return (newstate, token, endoftok)
+
+
+keywords = [
+    "case",
+    "class",
+    "else",
+    "esac",
+    "fi",
+    "if",
+    "in",
+    "inherits",
+    "isvoid",
+    "let",
+    "loop",
+    "new",
+    "not",
+    "of",
+    "pool",
+    "then",
+    "while"
+]
+
+
+def checkKeyword(tok):
+    if tok:
+        if tok[0] == "Identifier":
+            if tok[1] in keywords:
+                return ("Keyword", tok[1])
+    return tok
+
 
 def lexer(s):
     lexes = []
@@ -140,18 +223,23 @@ def lexer(s):
     if s == "":
         return []
         
+    #state = "Normal"
+    #states = readToken(state, s)
     state = "Normal"
-    states = readToken(state, s)
 
     while state != "EOF":
-        s = s[1:]
         states = readToken(state, s)
         state = states[0]
         tok = states[1]
+        tok = checkKeyword(tok)
+        moveon = not states[2]
         if tok:
             lexes.append(tok)
+        if moveon:
+            s = s[1:]
 
     return lexes
+
 
 def main():
     from sys import argv
@@ -160,7 +248,7 @@ def main():
         with open(fn, 'r') as f:
             ls = f.readlines()
             ls = '\n'.join(ls)
-        print(lexer(ls))
+        list(map(print, lexer(ls)))
 
 if __name__ == '__main__':
     main()
